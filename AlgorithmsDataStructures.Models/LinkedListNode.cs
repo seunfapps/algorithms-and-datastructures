59d83e43d5a58b8dad0e8801b347f19bb63a9c12
Oluwaseun Fapohunda <seun@Oluwaseuns-MacBook-Pro.local>
2020-04-12 08:14:42 -0700

    LinkedListNode Model

---
+++ AlgorithmsDataStructures.Models/LinkedListNode.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace AlgorithmsDataStructures.Models
+{
+    public class LinkedListNode<T>
+    {
+        public LinkedListNode(T value)
+        {
+            Value = value;
+        }
+        public T Value {get;set;}
+
+        public LinkedListNode<T> Next {get; set;}
+        
+    }
+}
