1a3a7657f74df48bb033e219a4fcb91ee6c07cf9
Oluwaseun Fapohunda <seun@Oluwaseuns-MacBook-Pro.local>
2020-04-12 03:42:32 -0700

    Simple LinkedList.

---
+++ AlgorithmsDataStructures.App/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using AlgorithmsDataStructures.Models;
+
+namespace AlgorithmsDataStructures.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node a = new Node{ Value = 10 };
+
+            Node b = new Node { Value = 12 };
+
+            Node c = new Node { Value =  15 };
+
+            a.Next = b;
+            b.Next = c;
+        }
+    }
+}
