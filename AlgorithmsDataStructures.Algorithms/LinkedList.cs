eaf7392eeac5da79979b7cc375bc31a1d7accf6b
Oluwaseun Fapohunda <seun@Oluwaseuns-MacBook-Pro.local>
2020-04-13 02:24:22 -0700

    LinkedList --AddFirst

--- AlgorithmsDataStructures.Algorithms/LinkedList.cs
+++ AlgorithmsDataStructures.Algorithms/LinkedList.cs
@@ -1,9 +1,72 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
+using AlgorithmsDataStructures.Models;
 
 namespace AlgorithmsDataStructures.Algorithms
 {
-    public class LinkedList : ICollection<T>
+    public class LinkedList<T> : System.Collections.Generic.ICollection<T>
     {
+        //first node
+        public LinkedListNode<T> Head { get; set; }
+        //last node
+        public LinkedListNode<T> Tail { get; private set; }
+
+        public int Count { get; private set; }
+
+        public bool IsReadOnly { get { return false; } }
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddFirst(T value)
+        {
+            AddFirst(new LinkedListNode<T>(value));
+        }
+
+        public void AddFirst(LinkedListNode<T> node)
+        {
+            LinkedListNode<T> temp = Head;
+
+            Head = node;
+
+            Head.Next = temp;
+
+            Count++;
+
+            if (Count == 1)
+                Tail = Head;
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.Collections.Generic.IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
