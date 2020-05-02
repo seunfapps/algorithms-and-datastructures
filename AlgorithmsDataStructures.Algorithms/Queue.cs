433b4a06a891c23ab520e2641ecf95854c243e4a
seunsaber <seunfapps@gmail.com>
2020-05-02 23:08:12 +0100

    Queue - Enqueue, Dequeue, Peek.

---
+++ AlgorithmsDataStructures.Algorithms/Queue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace AlgorithmsDataStructures.Algorithms
+{
+    public class Queue<T>
+    {
+        LinkedList<T> list = new LinkedList<T>();
+
+        public void Enqueue (T item)
+        {
+            list.AddLast(item);
+        }
+
+        public T Dequeue()
+        {
+            if (list.Count == 0)
+                throw new InvalidOperationException("The Queue is empty");
+
+            T value = list.Head.Value;
+            list.RemoveFirst();
+            return value;
+        }
+
+        public T Peek()
+        {
+            if (list.Count == 0)
+                throw new InvalidOperationException("The Queue is empty");
+
+            return list.Head.Value;
+        }
+    }
+}
