4ec71b7daf3754f275fcbd94fa2b03e77b51eb77
seunsaber <seunfapps@gmail.com>
2020-05-01 22:22:07 -0700

    DoublyLinkedList Model.

---
+++ AlgorithmsDataStructures.Models/DoublyLinkedListNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace AlgorithmsDataStructures.Models
+{
+    public class DoublyLinkedListNode<T>
+    {
+        public DoublyLinkedListNode(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+        public DoublyLinkedListNode<T> Next { get; set; }
+        public DoublyLinkedListNode<T> Previous { get; set; }
+
+    }
+}
