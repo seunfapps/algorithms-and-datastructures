b1624251ef21b80df5eb808653fdb83fe8c7d1da
seunsaber <seunfapps@gmail.com>
2020-05-02 21:19:21 +0100

    Stack - Push and Pop

---
+++ AlgorithmsDataStructures.Algorithms/Stack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace AlgorithmsDataStructures.Algorithms
+{
+    public class Stack<T>
+    {
+        public Stack()
+        {
+        }
+
+        //Implementing Stack using the LinkedList we created
+        LinkedList<T> list = new LinkedList<T>();
+
+
+        public void Push(T item)
+        {
+            list.AddFirst(item);
+        }
+
+        public T Pop()
+        {
+            if (list.Count == 0)
+                throw new InvalidOperationException("Stack is empty");
+
+            T value = list.Head.Value;
+            list.RemoveFirst();
+            return value;
+        }
+    }
+}
