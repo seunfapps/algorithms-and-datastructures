e2219a5dae7341068bbdd099bbef73670e4bbd9a
seunsaber <seunfapps@gmail.com>
2020-05-03 14:01:43 +0100

    BinaryTree - Model

---
+++ AlgorithmsDataStructures.Models/BinaryTreeNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace AlgorithmsDataStructures.Models
+{
+    public class BinaryTreeNode<T> : IComparable<T> where T : IComparable<T>
+    {
+        public BinaryTreeNode<T> Left { get; set; }
+        public BinaryTreeNode<T> Right { get; set; }
+        public T Value { get; set; }
+
+        public BinaryTreeNode(T value)
+        {
+            Value = value;
+        }
+
+        public int CompareTo(T other)
+        {
+            return Value.CompareTo(other);
+        }
+    }
+
+
+}
